0460b91a694a74cb26bffc56d5ac9533325562a0
Jason Allen <jason.thomas.allen@protonmail.com>
2023-02-05 16:48:10 +0800

    First UI Test

---
+++ GreetMe/GreetMeUITests/Page Objects/LoginPage.swift
@@ -0,0 +1,19 @@
+//
+//  LoginPage.swift
+//  GreetMeUITests
+//
+//  Created by Jason Allen on 5/2/2023.
+//
+
+import Foundation
+import XCTest
+
+class LoginPage {
+
+    var app: XCUIApplication
+    
+    init(app: XCUIApplication) {
+        self.app = app
+    }
+    
+}
