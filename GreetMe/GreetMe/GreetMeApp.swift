c1c7a78d1e34dadca612fd67e57029f066a4d9ba
Jason Allen <jason.thomas.allen@protonmail.com>
2023-02-05 17:19:24 +0800

    First UI Test - fix init issues.

--- GreetMe/GreetMe/GreetMeApp.swift
+++ GreetMe/GreetMe/GreetMeApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import GreetMeFramework
 
 @main
 struct GreetMeApp: App {
--- GreetMe/GreetMeFramework/GreetMeFramework.h
+++ GreetMe/GreetMeFramework/GreetMeFramework.h
@@ -1,1 +1,19 @@
+//
+//  PayMeFramework.h
+//  PayMeFramework
+//
+//  Created by Jason Allen on 4/2/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PayMeFramework.
+FOUNDATION_EXPORT double GreetMeFrameworkVersionNumber;
+
+//! Project version string for PayMeFramework.
+FOUNDATION_EXPORT const unsigned char GreetMeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GreetMeFramework/PublicHeader.h>
+
+
 
