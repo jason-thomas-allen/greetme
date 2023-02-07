53425dcc9a5e429f570dbbc1800ecc982b374bc2
Jason Allen <jason.thomas.allen@protonmail.com>
2023-02-07 09:14:23 +0800

    empty stub

--- GreetMe/GreetMe/Stub Services/StubAuthService.swift
+++ GreetMe/GreetMe/Stub Services/StubAuthService.swift
@@ -9,10 +9,5 @@ import Foundation
 import GreetMeFramework
 
 class StubAuthService: AuthService {
-    func loginAsync(username: String, password: String) async throws {
-        if username == "43906621" && password == "Password" {
-        } else {
-            throw(AuthError.notAuthenticated)
-        }
-    }
+    
 }
