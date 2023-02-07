a654e60603c51b0c6e7e179ae3a2a6797d7e9d3b
Jason Allen <jason.thomas.allen@protonmail.com>
2023-02-07 08:43:41 +0800

    First UI test passes - hidden stub

---
+++ GreetMe/GreetMe/Stub Services/StubAuthService.swift
@@ -0,0 +1,18 @@
+//
+//  StubAuthService.swift
+//  GreetMe
+//
+//  Created by Jason Allen on 6/2/2023.
+//
+
+import Foundation
+import GreetMeFramework
+
+class StubAuthService: AuthService {
+    func loginAsync(username: String, password: String) async throws {
+        if username == "43906621" && password == "Password" {
+        } else {
+            throw(AuthError.notAuthenticated)
+        }
+    }
+}
